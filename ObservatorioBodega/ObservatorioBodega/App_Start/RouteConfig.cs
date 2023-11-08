c790baf0d4c6717e92c51ac1693acd053b89a33c
Natsuho-1 <santosronaldolemustorres@gmail.com>
2023-11-08 12:43:12 -0600

    Integracion menu y cruds

--- ObservatorioBodega/ObservatorioBodega/App_Start/RouteConfig.cs
+++ ObservatorioBodega/ObservatorioBodega/App_Start/RouteConfig.cs
@@ -9,29 +9,41 @@ namespace ObservatorioBodega
 {
     public class RouteConfig
     {
+
+        //para probar funcion debido a fallo del enrutador de abajo
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-
             routes.MapRoute(
-               name: "Menus",
-               url: "{controller}/{action}/{id}",
-               defaults: new { controller = "Menus", action = "Index", id = UrlParameter.Optional }
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
-
-            routes.MapRoute(
-               name: "Documentos",
-               url: "{controller}/{action}/{id}",
-               defaults: new { controller = "Documentos", action = "Index", id = UrlParameter.Optional }
-            );
-
-            //RUTA PARA COLABORADORES
-            routes.MapRoute(
-            name: "Colaboradores",
-            url: "{controller}/{action}/{id}",
-            defaults: new { controller = "Colaboradores", action = "Index", id = UrlParameter.Optional }
-);
         }
+        //        public static void RegisterRoutes(RouteCollection routes)
+        //        {
+        //            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+
+        //            routes.MapRoute(
+        //               name: "Menus",
+        //               url: "{controller}/{action}/{id}",
+        //               defaults: new { controller = "Menus", action = "Index", id = UrlParameter.Optional }
+        //            );
+
+        //            routes.MapRoute(
+        //               name: "Documentos",
+        //               url: "{controller}/{action}/{id}",
+        //               defaults: new { controller = "Documentos", action = "Index", id = UrlParameter.Optional }
+        //            );
+
+        //            //RUTA PARA COLABORADORES
+        //            routes.MapRoute(
+        //            name: "Colaboradores",
+        //            url: "{controller}/{action}/{id}",
+        //            defaults: new { controller = "Colaboradores", action = "Index", id = UrlParameter.Optional }
+        //);
+        //        }
     }
 }
